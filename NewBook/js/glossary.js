f633d4f9fcf5809f9b85d5953cdeb00c390c76e6
Tony Montana <junggeorge@yandex.ru>
2014-03-12 20:31:45 +0400

    some changes in NewBook

--- NewBook/js/glossary.js
+++ NewBook/js/glossary.js
@@ -6,106 +6,135 @@ var glossary =
 	init: function(rus_db, eng_db)
 	{
 		var _t = this;
-		var gl = ge("glossary");
+		
+		_t.rdb = rus_db.sort(_t.compareString(_t.rus_abc));
+		_t.edb = eng_db.sort(_t.compareString(_t.eng_abc));
+		
+		_t.gl = ge("glossary");
+		_t.currentList = null;
 		
 		var eng_btn = document.createElement("button");
 		eng_btn.innerHTML = "ENG";
 		addClass(eng_btn, "red");
-		var rus_div = _t.create(rus_db, _t.rus_abc, eng_btn);
-		gl.appendChild(rus_div);
+		var rus_div = _t.createAbc(_t.rus_abc, _t.rdb);
+		rus_div.appendChild(eng_btn);
+		_t.gl.appendChild(rus_div);
 		
 		var rus_btn = document.createElement("button");
 		rus_btn.innerHTML = "РУС";
 		addClass(rus_btn, "red");
-		var eng_div = _t.create(eng_db, _t.eng_abc, rus_btn);
-		gl.appendChild(eng_div);
+		var eng_div = _t.createAbc(_t.eng_abc, _t.edb);
+		eng_div.appendChild(rus_btn);
+		_t.gl.appendChild(eng_div);
 		
 		rus_div.style.display = "block";
 		eng_div.style.display = "none";
 		
-		var f = function(e, r)
-		{
-			return function()
+		eng_btn.onclick = function(r, e)
 			{
-				toggle(e);
-				toggle(r);
-			};
-		}(eng_div, rus_div);
-		
-		eng_btn.onclick = f;
-		rus_btn.onclick = f;
+				return function()
+					{
+						toggle(r);
+						toggle(e);
+						if(_t.active_button)
+							removeClass(_t.active_button, "red");
+						addClass(e.firstChild, "red");
+						_t.active_button = e.firstChild;
+						_t.showLetterFrom(0, _t.eng_abc, _t.edb);
+					};
+			}(rus_div, eng_div);
+		rus_btn.onclick = function(r, e)
+			{
+				return function()
+					{
+						toggle(r);
+						toggle(e);
+						if(_t.active_button)
+							removeClass(_t.active_button, "red");
+						addClass(r.firstChild, "red");
+						_t.active_button = r.firstChild;
+						_t.showLetterFrom(0, _t.rus_abc, _t.rdb);
+					};
+			}(rus_div, eng_div);
+			
+		addClass(rus_div.firstChild, "red");
+		_t.active_button = rus_div.firstChild;
+		_t.showLetterFrom(0, _t.rus_abc, _t.rdb);
 	},
 	
-	create: function(db, abc, btn)
+	createAbc: function(abc, db)
 	{
 		var _t = this;
-		var list_db = document.createElement("div");
-		var list_abc = document.createElement("div");
 		
-		db.sort(_t.compareString(abc));
-				
-		var ol, si = 1;
+		var div = document.createElement("div");
+		var cletter = -1;
 		for(var i = 0; i < db.length; i++)
 		{
-			var f = false;
-			if(i > 0)
+			var letter = 1+2*Math.floor(abc.indexOf(db[i].title.charAt(0))/2);
+			if(cletter != letter)
 			{
-				var ai = Math.floor(abc.indexOf(db[i-1].title.charAt(0)) / 2);
-				var bi = Math.floor(abc.indexOf(db[i].title.charAt(0)) / 2);
-				f = ai != bi;
-			}
-			if(i == 0 || f)
-			{
-				var h = document.createElement("a");
-				h.name = "letter_start" + i;
-				h.id = "letter_start" + i;
-				h.href = "#body";
-				h.className = "start_letter";
-				h.innerHTML = db[i].title.charAt(0);
-				
-				var b = document.createElement("span");
-				b.innerHTML = "На верх&uarr;";
-				h.appendChild(b);
-				
-				list_db.appendChild(h);
-				ol = document.createElement("ol");
-				ol.start = i+1;
-				list_db.appendChild(ol);
-				
-				var fj = Math.floor(abc.indexOf(db[i].title.charAt(0)) / 2) * 2 + 1;
-				for(var j = si; j < fj; j += 2)
+				for(var j = cletter + 2; j < letter; j += 2)
 				{
-					var letter = document.createElement("span");
-					letter.innerHTML = abc.charAt(j);
-					list_abc.appendChild(letter);
+					var span = document.createElement("span");
+					span.innerHTML = abc.charAt(j);
+					div.appendChild(span);
 				}
-				var letter = document.createElement("a");
-				addClass(letter, "button");
-				letter.href = "#letter_start" + i;
-				letter.innerHTML = abc.charAt(fj);
-				list_abc.appendChild(letter);
-				si = fj+2;
+				var btn = document.createElement("button");
+				btn.innerHTML = abc.charAt(letter);
+				btn.onclick = function(s)
+					{
+						return function(e)
+						{
+							if(_t.active_button)
+								removeClass(_t.active_button, "red");
+							addClass(this, "red");
+							_t.active_button = this;
+							_t.showLetterFrom(s, abc, db);
+						}
+					}(i);
+				div.appendChild(btn);
+				cletter = letter;
 			}
+		}
+		for(var j = cletter + 1; j < abc.length; j += 2)
+		{
+			var span = document.createElement("span");
+			span.innerHTML = abc.charAt(j);
+			div.appendChild(span);
+		}
+		
+		return div;
+	},
+	
+	showLetterFrom: function(start, abc, db)
+	{
+		var _t = this;
+		/*var abc = iseng ? _t.eng_abc : _t.rus_abc;
+		var db = iseng ? _t.edb : _t.rdb;*/
+				
+		if(_t.currentList)
+			_t.gl.removeChild(_t.currentList);
+			
+		_t.currentList = document.createElement("ol");
+		_t.gl.appendChild(_t.currentList);
+		
+		var i = start;
+		var letter = Math.floor(abc.indexOf(db[i].title.charAt(0)) / 2);
+		do
+		{
 			var a = document.createElement("a");
 			a.innerHTML = db[i].title;
 			a.href = db[i].href;
 			var li = document.createElement("li");
 			li.appendChild(a);
-			ol.appendChild(li);
+			_t.currentList.appendChild(li);
+			
+			i++;
 		}
-		for(var j = si; j < abc.length; j += 2)
-		{
-			var letter = document.createElement("span");
-			letter.innerHTML = abc.charAt(j);
-			list_abc.appendChild(letter);
-		}
-		list_abc.appendChild(btn);
-		
-		var div = document.createElement("div");
-		div.appendChild(list_abc);
-		div.appendChild(list_db);
-		return div;
+		while(i < db.length &&
+				Math.floor(abc.indexOf(db[i].title.charAt(0)) / 2) == letter);
 	},
+	
 	compareString: function(abc)
 	{
 		return function(a, b)
